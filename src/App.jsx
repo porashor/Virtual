4a713caf989d5489c87489fa3ee67a79eb38d4ac
Parashar Das <porashorbabu@gmail.com>
2024-05-30 23:47:44 +0600

    all task or 1st day

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-black'>
+      hello world
+      <span className='text-blue-700'>ddk</span>
+    </div>
+  )
+}
+
+export default App
